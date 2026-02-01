fba0f8f5d33d8b813c3f0f55d8ff12f590574aea
Dean <deanlt.santos@gmail.com>
2026-02-02 07:50:34 +0800

    created a function called showToDos and implemented the business logic inside the function

--- lib/todo_cli/todos.ex
+++ lib/todo_cli/todos.ex
@@ -1,3 +1,11 @@
 defmodule ToDoLogic do
+  alias ToDoInterface
 
+  def showToDos(tasks) do
+    IO.puts("List of tasks")
+    Enum.each(Enum.with_index(tasks), fn {task, index} ->
+      IO.puts("#{index}. #{task}")
+    end)
+    ToDoInterface.run(tasks)
+  end
 end
