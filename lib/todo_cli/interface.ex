f396c24c835d18d0d69f1e8867f2a260e1489623
Dean <deanlt.santos@gmail.com>
2026-02-02 05:28:07 +0800

    add a switch case for the user's choice

--- lib/todo_cli/interface.ex
+++ lib/todo_cli/interface.ex
@@ -5,8 +5,19 @@ defmodule ToDoInterface do
     [2] Add Note
     [3] Modify Note
     [4] Delete Note
+    [5] Quit
     ")
+
     choice = IO.gets(">:")  |> String.trim()
     IO.puts(choice)
+
+    case choice do
+      "1" -> IO.puts(:"See all notes")
+      "2" -> IO.puts(:"Add Note")
+      "3" -> IO.puts(:"Modify note")
+      "4" -> IO.puts(:"Delete Note")
+      "5" -> IO.puts(:"Quit")
+    end
+
   end
 end
