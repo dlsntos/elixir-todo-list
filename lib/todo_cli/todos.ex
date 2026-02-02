546ccf85a9723932acf24b3dc544e35d1d4380ca
Dean <deanlt.santos@gmail.com>
2026-02-02 08:32:46 +0800

    modified the print message in the if condition

--- lib/todo_cli/todos.ex
+++ lib/todo_cli/todos.ex
@@ -5,7 +5,7 @@ defmodule ToDoLogic do
     IO.puts("List of tasks")
     Enum.each(Enum.with_index(tasks), fn {task, index} ->
       if length(tasks) === 1 do
-        IO.puts("No")
+        IO.puts("No Tasks Available right now")
       else
         unless index === 0 do
             IO.puts("#{index}. #{task}")
